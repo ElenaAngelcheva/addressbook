f01a9716af7888fa9226c2f37ea742752da94a7a
Elena <angelcheva_elena@bk.ru>
2019-03-03 17:04:51 +0300

    Привела проект к правильной структуре

---
+++ src/test/java/ru/rt/client/model/GroupDate.java
@@ -0,0 +1,25 @@
+package ru.rt.client.model;
+
+public class GroupDate {
+    private String name;
+    private final String header;
+    private final String footer;
+
+    public GroupDate(String name, String header, String footer) {
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+}
---
+++ src/test/java/ru/rt/client/tests/GroupDeletionTest.java
@@ -0,0 +1,13 @@
+package ru.rt.client.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTest extends TestBase {
+
+    @Test
+    public void testGroupDeletion() throws Exception {
+        app.navToGroupsPage();
+        app.selectGroup();
+        app.clickDeleteBtn();
+    }
+}
